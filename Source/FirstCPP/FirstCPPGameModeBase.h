f564bf05c90f836940c936589539e9cfb774a405
Kyner <github@kghm123.com>
2018-07-01 16:26:06 +0900

    Initial Commit

---
+++ Source/FirstCPP/FirstCPPGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FirstCPPGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FIRSTCPP_API AFirstCPPGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
