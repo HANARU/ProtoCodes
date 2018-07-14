425120e6dbd6427f5617db7fb101d3d278462667
Kyner <github@kghm123.com>
2018-07-14 21:30:00 +0900

    Change Target & Add PositonReport

--- Source/FirstCPP/Private/PositionReport.cpp
+++ Source/FirstCPP/Private/PositionReport.cpp
@@ -1,7 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "FirstCPP.h"
 #include "PositionReport.h"
+#include "Gameframework/Actor.h"
+#include "Vector.h"
+#include "FirstCPP.h"
 
 
 // Sets default values for this component's properties
@@ -11,7 +13,7 @@ UPositionReport::UPositionReport()
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
 
-	// ...
+	
 }
 
 
@@ -20,7 +22,10 @@ void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
+	FString ObjectName = GetOwner()->GetName(); 
+	FString ObjectPos = GetOwner()->GetActorLocation().ToString();
+	UE_LOG(LogTemp, Warning, TEXT("Position report for duty on %s"), *ObjectName);
+	UE_LOG(LogTemp, Error, TEXT("%s is at %s"), *ObjectName, *ObjectPos);
 	
 }
 
--- Source/FirstCPPEditor.Target.cs
+++ Source/FirstCPPEditor.Target.cs
@@ -9,6 +9,9 @@ public class FirstCPPEditorTarget : TargetRules
 	{
 		Type = TargetType.Editor;
 
+        bUseUnityBuild = false;
+        bUsePCHFiles = false;
+
 		ExtraModuleNames.AddRange( new string[] { "FirstCPP" } );
 	}
 }
